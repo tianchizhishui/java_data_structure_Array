c6484c55c1d6c05f6414e43a94e44bc3c1aaa257
Tianchi (David) Liu <55475915+tianchizhishui@users.noreply.github.com>
2019-10-15 19:42:31 -0700

    unfinished sorted array

---
+++ SortedArray/src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner scanner= new Scanner(System.in);
+    public static void main(String[] args) {
+
+        int[] myIntegers = getIntegers(5);
+    }
+
+    public static int[] getIntegers(int capacity) {
+        int[] array = new int[capacity];
+        System.out.println("Enter " + capacity + " integer values: \r");
+        for(int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
+        }
+        return array;
+    }
+    public static void printArray(int[] array) {
+        for (int i= 0; i < array.length; i++) {
+            System.out.println("Element" + i + " contents " + array[i]);
+        }
+    }
+
+    public static int[] sortIntegers(int[] array) {
+        int[] sortedArray = new int[array.length];
+        for(int i = 0; i < array.length; i++) {
+            sortedArray[i] = array[i];
+        }
+        boolean flag = true;
+        int temp;
+
+
+    }
+}
